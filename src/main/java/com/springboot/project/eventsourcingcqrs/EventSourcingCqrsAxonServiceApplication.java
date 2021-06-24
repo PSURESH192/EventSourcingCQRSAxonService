b848f0a009499d2ddaf4afb5c29138417e7e5a5e
Suresh_Prabha <suresh.uday92@gmail.com>
2021-06-24 17:06:40 +0530

    Changes

--- src/main/java/com/springboot/project/eventsourcingcqrs/EventSourcingCqrsAxonServiceApplication.java
+++ src/main/java/com/springboot/project/eventsourcingcqrs/EventSourcingCqrsAxonServiceApplication.java
@@ -1,9 +1,13 @@
 package com.springboot.project.eventsourcingcqrs;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@Slf4j
+@EnableEurekaClient
 @SpringBootApplication
 public class EventSourcingCqrsAxonServiceApplication {
 
